aa05e78b250e9346579afe0b6e294eef94a84556
Mattias EDMAN <MEDM01@aurobay.com>
2023-10-18 14:15:58 +0200

    modified test.js with checks and threshold

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
+
 export const options = {
   vus: 1,
-  duration: '15s',
+  duration: '5s',
+
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<1000']
+  },
 };
+
 export default function () {
-  http.get('http://test.k6.io');
+
+  const res = http.get('http://test.k6.io');
+  check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
+
+  
+
 }

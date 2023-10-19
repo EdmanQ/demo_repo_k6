9e9e5809975e5ee248d55f7d7881d682746e11b0
Mattias EDMAN <MEDM01@aurobay.com>
2023-10-19 14:52:52 +0200

    update test.js and added integration tests in functionalTestwithk6

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+
+//This is the load model
 export const options = {
   vus: 1,
   duration: '5s',
 
+//This failes the load test if success rate is not 99.99% or 95% of the request have avg.duration less than 2500ms
   thresholds: {
     http_req_failed: ['rate<0.01'],
     http_req_duration: ['p(95)<2500']

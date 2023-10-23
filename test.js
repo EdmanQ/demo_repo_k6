7615bd575f9c355e92351912494009178710f5d7
Mattias EDMAN <MEDM01@aurobay.com>
2023-10-23 10:06:23 +0200

    Removed functional integration tests - updated test.js

--- test.js
+++ test.js
@@ -2,18 +2,19 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 
-//This is the load model
+//Load model
 export const options = {
   vus: 1,
   duration: '5s',
 
-//This failes the load test if success rate is not 99.99% or 95% of the request have avg.duration less than 2500ms
+//This fail the load test if success rate is not 99.99% or 95% of the request have avg.duration less than 2500ms
   thresholds: {
     http_req_failed: ['rate<0.01'],
     http_req_duration: ['p(95)<2500']
   },
 };
 
+//Test
 export default function () {
 
   const res = http.get('https://swapi.dev/api/people/2');
